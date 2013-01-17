f8616a1c49a79eaae710a6f3902014b2376a8c53
danieru <danieru.dressler@gmail.com>
2013-01-17 07:17:56 -0700

    Fix wrong option in makefile

--- jni/Application.mk
+++ jni/Application.mk
@@ -3,7 +3,7 @@
 APP_STL := gnustl_static
 APP_ABI := armeabi armeabi-v7a x86
 # For releases
-APP_CFLAGS := 0fast -ftree-vectorize -fsingle-precision-constant -fvariable-expansion-in-unroller -ffast-math -funroll-loops -fno-math-errno -funsafe-math-optimizations -ffinite-math-only -fdata-sections -fbranch-target-load-optimize2 -fno-exceptions -fno-stack-protector -flto -fforce-addr -funswitch-loops -ftree-loop-im -ftree-loop-ivcanon -fivopts -Wno-psabi
+APP_CFLAGS := -Ofast -ftree-vectorize -fsingle-precision-constant -fvariable-expansion-in-unroller -ffast-math -funroll-loops -fno-math-errno -funsafe-math-optimizations -ffinite-math-only -fdata-sections -fbranch-target-load-optimize2 -fno-exceptions -fno-stack-protector -flto -fforce-addr -funswitch-loops -ftree-loop-im -ftree-loop-ivcanon -fivopts -Wno-psabi
 APP_LDFLAGS := -flto
 APP_PLATFORM := android-9
 
